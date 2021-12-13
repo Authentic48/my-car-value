7e0708dc6ccb5e1462333b51d2a807c413c07c15
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-12-13 09:55:48 +0000

    CurrentUser Interceptor and decorator added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,25 +8,29 @@ import {
   Query,
   Delete,
   Session,
+  UseInterceptors,
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
-
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { CurrentUserInterceptor } from './interceptors/serialize.interceptor';
+import { User } from './user.entity';
 
 @Controller('users')
 @Serialize(UserDto)
+@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
     private authService: AuthService,
   ) {}
   @Get('/current-user')
-  getCurrentSignInUser(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
+  getCurrentSignInUser(@CurrentUser() user: User) {
+    return user;
   }
   @Post('/signout')
   signOut(@Session() session: any) {
