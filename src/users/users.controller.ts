b349baf9c42a539debaed2a9660e00f083114b56
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-12-13 10:03:25 +0000

    interceptor refactor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,6 @@ import {
   Query,
   Delete,
   Session,
-  UseInterceptors,
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -17,12 +16,10 @@ import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { CurrentUserInterceptor } from './interceptors/serialize.interceptor';
 import { User } from './user.entity';
 
 @Controller('users')
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
